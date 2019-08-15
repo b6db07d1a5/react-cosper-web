9fa5bad0fd7ebecbe5837b13aab1b58f58f52383
Ananass <sitthichai@tencent.co.th>
2019-08-15 22:33:48 +0700

    Add point locate

--- src/components/trend/TrendMap.js
+++ src/components/trend/TrendMap.js
@@ -4,18 +4,61 @@ import { Layout, List } from 'antd'
 const { Content } = Layout;
 
 const data = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 ]
 
 class TrendMap extends Component {
-    state = {}
-    render() {
-        return (
-            <center>
-                <Content style={{ borderTop: '1px solid #ddd' }}>
-                    <img src='static/trend/map/thai_yellow.png' />
-                    {/* <div style={{
+	state = {}
+	render() {
+
+		const x = 4;
+		const y = 0;
+
+		const padd = 50;
+
+		const arr = [
+			{ xs: 3, xe: 3, y: 0 },
+			{ xs: 1, xe: 4, y: 1 },
+			{ xs: 1, xe: 7, y: 2 },
+			{ xs: 2, xe: 7, y: 3 },
+			{ xs: 2, xe: 8, y: 4 },
+			{ xs: 2, xe: 8, y: 5 },
+			{ xs: 3, xe: 5, y: 6 },
+			{ xs: 3, xe: 5, y: 7 },
+			{ xs: 3, xe: 3, y: 8 },
+
+			{ xs: 2, xe: 2, y: 9 },
+			{ xs: 2, xe: 3, y: 10 },
+			{ xs: 2, xe: 4, y: 11 },
+			{ xs: 3, xe: 4, y: 12 },
+
+		]
+		return (
+			<center>
+				<Content style={{ borderTop: '1px solid #ddd' }}>
+					<div style={{
+						paddingTop: padd, paddingBottom: padd,
+						backgroundColor: '#fff',
+						width: 960,
+						textAlign: 'center',
+						position: 'relative',
+					}}>
+						<img src='static/trend/map/thai_yellow.png' />
+						<ImageAbsolute x={3} y={0} />
+						<ImageAbsolute x={4} y={1} />
+						<ImageAbsolute x={7} y={2} />
+						<ImageAbsolute x={7} y={3} />
+						<ImageAbsolute x={8} y={4} />
+						<ImageAbsolute x={8} y={5} />
+						<ImageAbsolute x={5} y={6} />
+						<ImageAbsolute x={5} y={7} />
+						<ImageAbsolute x={3} y={8} />
+						<ImageAbsolute x={2} y={9} />
+						<ImageAbsolute x={3} y={10} />
+						<ImageAbsolute x={4} y={11} />
+						<ImageAbsolute x={4} y={12} />
+					</div>
+					{/* <div style={{
                         textAlign: 'center',
                         position: 'relative',
                         backgroundColor: '#fff',
@@ -33,46 +76,61 @@ class TrendMap extends Component {
                             })
                         } 
                     </div> */}
-                </Content >
-            </center >
-        );
-    }
+				</Content >
+			</center >
+		);
+	}
 }
 
+
+{/* <ImageAbsolute x={3} y={0} />
+<ImageAbsolute x={4} y={1} />
+<ImageAbsolute x={7} y={2} />
+<ImageAbsolute x={7} y={3} />
+<ImageAbsolute x={8} y={4} />
+<ImageAbsolute x={8} y={5} />
+<ImageAbsolute x={5} y={6} />
+<ImageAbsolute x={5} y={7} />
+<ImageAbsolute x={3} y={8} />
+<ImageAbsolute x={2} y={9} />
+<ImageAbsolute x={3} y={10} />
+<ImageAbsolute x={4} y={11} />
+<ImageAbsolute x={4} y={12} /> */}
+
 const face = [
-    '/static/trend/face_3.png',
-    '/static/trend/face_3.png'
+	'/static/trend/face_3.png',
+	'/static/trend/face_3.png'
 ]
 
 function getRandom() {
-    let ranArr = [];
+	let ranArr = [];
 
 }
 
 function ImageAbsolute({ x, y }) {
-    const size = rnd(10, 150);
-    const img = face[rnd(0, 1)]
+	const size = rnd(100, 100);
+	const img = face[rnd(0, 1)]
 
-    return <div style={{
-        position: 'absolute',
-        left: `${x}0%`,
-        top: `${y}0%`,
-    }}>
-        <div>#facebook</div>
-        <img src={img} width={size} height={size} />
-    </div>
+	return <div style={{
+		position: 'absolute',
+		left: `${x}00px`,
+		top: `${y}00px`,
+	}}>
+		<div>#facebook</div>
+		<img src={img} width={size} height={size} />
+	</div>
 }
 
 function rand(max, min) {
-    return Math.floor(Math.random() * (max - min) + max)
+	return Math.floor(Math.random() * (max - min) + max)
 }
 
 function rnd(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function randy() {
-    return Math.floor(Math.random() * (6 - 1) + 6)
+	return Math.floor(Math.random() * (6 - 1) + 6)
 }
 
 export default TrendMap;
