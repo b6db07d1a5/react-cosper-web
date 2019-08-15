4bd1e12899212b5c58e7daa3b399da108ff51758
Ananass <sitthichai@tencent.co.th>
2019-08-15 23:06:10 +0700

    Add trend located

--- src/components/trend/TrendMap.js
+++ src/components/trend/TrendMap.js
@@ -8,14 +8,11 @@ const data = [
 ]
 
 class TrendMap extends Component {
-	state = {}
-	render() {
+	state = {
+		imageElement: []
+	}
 
-		const x = 4;
-		const y = 0;
-
-		const padd = 50;
-
+	componentDidMount() {
 		const arr = [
 			{ xs: 3, xe: 3, y: 0 },
 			{ xs: 1, xe: 4, y: 1 },
@@ -26,13 +23,34 @@ class TrendMap extends Component {
 			{ xs: 3, xe: 5, y: 6 },
 			{ xs: 3, xe: 5, y: 7 },
 			{ xs: 3, xe: 3, y: 8 },
-
 			{ xs: 2, xe: 2, y: 9 },
 			{ xs: 2, xe: 3, y: 10 },
 			{ xs: 2, xe: 4, y: 11 },
-			{ xs: 3, xe: 4, y: 12 },
-
+			{ xs: 3, xe: 4, y: 12 }
 		]
+
+		let imageElement = [];
+		let count = 1;
+		arr.forEach(function ({ xs, xe, y }) {
+			let xcount = xe;
+
+			while (xcount >= xs) {
+				imageElement.push(<ImageAbsolute x={xcount} y={y} />)
+				count++;
+				xcount--;
+			}
+		})
+
+		console.log('element total = ', count)
+		this.setState({
+			imageElement
+		})
+	}
+	render() {
+
+		const { imageElement } = this.state
+		const padd = 50;
+
 		return (
 			<center>
 				<Content style={{ borderTop: '1px solid #ddd' }}>
@@ -44,7 +62,12 @@ class TrendMap extends Component {
 						position: 'relative',
 					}}>
 						<img src='static/trend/map/thai_yellow.png' />
-						<ImageAbsolute x={3} y={0} />
+						{
+							imageElement.map(function (ele) {
+								return ele
+							})
+						}
+						{/* <ImageAbsolute x={3} y={0} />
 						<ImageAbsolute x={4} y={1} />
 						<ImageAbsolute x={7} y={2} />
 						<ImageAbsolute x={7} y={3} />
@@ -56,7 +79,7 @@ class TrendMap extends Component {
 						<ImageAbsolute x={2} y={9} />
 						<ImageAbsolute x={3} y={10} />
 						<ImageAbsolute x={4} y={11} />
-						<ImageAbsolute x={4} y={12} />
+						<ImageAbsolute x={4} y={12} /> */}
 					</div>
 					{/* <div style={{
                         textAlign: 'center',
