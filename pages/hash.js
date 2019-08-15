48aac37502bc3e739bddf4a82f6493f09616eed8
Ananass <sitthichai@tencent.co.th>
2019-08-15 17:20:23 +0700

    Change HashJam layout

--- pages/hash.js
+++ pages/hash.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react'
+import { Layout, List } from 'antd'
 import Page from '@components/Page'
 import HashJam from '@components/hash/HashJam'
 import HashList from '@components/hash/HashList'
+
+const { Header, Content, Sider } = Layout;
+
+
+const data = [
+    '#Racing car.',
+    '#Japanese.',
+    '#100km after.',
+    '#Man charged.',
+    '#Los Angeles.',
+];
 
 class Hash extends Component {
     state = {}
 
     render() {
-        return (<Page>
-            <HashJam />
-            <HashList />
-        </Page>);
+        return (
+            <Page>
+                <Layout style={{ padding: '0 250px', borderTop: '1px solid #ddd' }}>
+                    <Content style={{ padding: '0 20px' }}>
+                        <div style={{ background: '#fff', padding: 24 }}>
+                            <HashJam />
+                            <HashList />
+                        </div>
+                    </Content>
+                    <Sider style={{ marginTop: '20px', backgroundColor: '#f0f2f5' }}>
+                        <h3>Hash Trends</h3>
+                        <List
+                            size="small"
+                            footer={<div>Footer</div>}
+                            bordered
+                            dataSource={data}
+                            renderItem={item => <List.Item>{item}</List.Item>}
+                        />
+                    </Sider>
+                </Layout>
+            </Page>
+        );
     }
 }
 
--- src/components/Page.js
+++ src/components/Page.js
@@ -6,14 +6,6 @@ import { Layout, Menu, List } from 'antd';
 
 const { Header, Content, Sider } = Layout;
 
-const data = [
-    '#Racing car.',
-    '#Japanese.',
-    '#100km after.',
-    '#Man charged.',
-    '#Los Angeles.',
-];
-
 const Page = ({ children }) => {
     return (<Layout className="layout" style={{ height: '100vh' }}>
         <Header style={{ backgroundColor: 'white' }} >
@@ -33,28 +25,9 @@ const Page = ({ children }) => {
                 </Menu>
             </center>
         </Header>
-        <Layout style={{ padding: '0 250px', borderTop: '1px solid #ddd' }}>
-            <Content style={{ padding: '0 20px' }}>
-                <div style={{ background: '#fff', padding: 24 }}>
-                    {children}
-                </div>
-            </Content>
-            <Sider style={{ marginTop: '20px', backgroundColor: '#f0f2f5' }}>
-                <h3>Hash Trends</h3>
-                <StyledList
-                    size="small"
-                    footer={<div>Footer</div>}
-                    bordered
-                    dataSource={data}
-                    renderItem={item => <List.Item>{item}</List.Item>}
-                />
-            </Sider>
-        </Layout>
+        {children}
     </Layout>);
 }
 
-const StyledList = styled(List)`
-
-`
 
 export default Page;
